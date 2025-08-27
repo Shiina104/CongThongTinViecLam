8f9560e485b98a925c3449644fa525dcc2598bfc
Nqq104 <nqq1104@gmail.com>
2025-08-27 13:49:51 +0700

    CTVL-2

---
+++ CongTinViecLam/CongTinViecLam/tests/test_login.py
@@ -0,0 +1,18 @@
+import unittest
+
+from CongTinViecLam import dao
+
+
+class TestLogin(unittest.TestCase):
+    def test_case_1(self):
+        self.assertTrue(dao.auth_user("user", "123"))
+
+    def test_case_2(self):
+        self.assertFalse(dao.auth_user("user", "456"))
+
+    def test_case_3(self):
+        self.assertFalse(dao.auth_user("user", 123))
+
+
+if __name__=="__main__":
+    unittest.main()
